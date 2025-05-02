05e382d9c99a9c008e6b3bb1fbf5005809c1905d
Pagayanan <pagayanancris.1@gmail.com>
2025-05-02 15:02:31 +0800

    Update CSS/Icon 3:02PM 5/2/2025

--- Sample.php
+++ Sample.php
@@ -30,8 +30,9 @@ try {
     <head>
         <meta charset="UTF-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="icon" type="image/x-icon" href="/img/psa logo.png">
         <title>Digital Library</title>
-        <link rel="icon" href="/favicon.ico" type="image/x-icon">
+
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet">
         <link href='https://unpkg.com/boxicons@2.1.4/css/boxicons.min.css' rel='stylesheet'>
         <script src="https://cdnjs.cloudflare.com/ajax/libs/pdf.js/3.4.120/pdf.min.js"></script>
